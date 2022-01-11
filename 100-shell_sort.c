8a7adf582f788d4edc367c59e824aa55c5c302ad
kalom60 <kaleabtesfaye00@gmail.com>
2022-01-11 02:09:08 -0800

    fix betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,48 +1,34 @@
 #include "sort.h"
 
 /**
- * shell_sort - function that sorts an array
- * of integers using the Shell sort algorithm
+ * shell_sort - sorts an array of integers in ascending order using the
+ * Shell sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
  *
- * @array: input arrray
- * @size: size of the array
- * Return: no return
+ * Return: void
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t n = 1;
+	size_t i, j, gap = 1;
+	int insert;
 
-	while (n < size)
-		n = (n * 3) + 1;
+	if (array == NULL || size < 2)
+		return;
 
-	while ((n = (n - 1) / 3) > 0)
-		_ssort(array, size, n);
-}
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-/**
- * _ssort - auxiliar function for
- * shell_sort function
- * @a: input arrray
- * @size: size of the array
- * @n: intervale
- * Return: no return
- */
-void _ssort(int *a, int size, int n)
-{
-	int t, i, j;
-
-	for (i = 0; (i + n) < size; i++)
+	while (gap > 0)
 	{
-		for (j = i + n; (j - n) >= 0; j = j - n)
+		for (i = gap; i < size; i++)
 		{
-			if (a[j] < a[j - n])
-			{
-				t = a[j];
-				a[j] = a[j - n];
-				a[j - n] = t;
-			}
+			insert = array[i];
+			for (j = i; j >= gap && array[j - gap] > insert; j = j - gap)
+				array[j] = array[j - gap];
+			array[j] = insert;
 		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
 	}
-	print_array(a, size);
 }
-
